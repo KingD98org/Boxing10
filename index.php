0bbd84a9e9ee5707dee1f0f35ff7ee3df2d99aba
KingD98 <sana.ym2@mail.ru>
2015-12-27 22:47:25 +0300

    123123
    23123123

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Web 1.0</title>
+        <link type="text/css" rel="stylesheet" href="main.css"/>
+		<link rel="shortcut icon" href="paper_plane.ico" type="image/x-icon">
+   
+    </head>	
+    
+	
+	
+	
+	<body>
+    
+	 <div class="menu">
+	 <div class="menubut">
+	 <a>О компании</a>
+	 <a>История</a>
+	 <a>Команда</a>
+	 <a>Контакты</a>
+	 </div>
+	 <div class="menubut2">
+	 <a>Услуги</a>
+	 <a>Проектирование и ИТ консалтинг</a>
+	 <a>ИТ аутсорсинг</a>
+	 <a>IP телефония</a>
+	 <a>веб ателье</a>
+	 </div>
+	 <div class="menubut4">
+	 <a>Клиенты</a>
+	 <a>Обработанные заказы</a>
+	 <a>Примеры работ</a>
+	 <a>Готовые эскизы</a>
+	 </div>
+	 <div class="menubut3">
+	 <a>Новости</a>
+	 <a>Скидки</a>
+	 <a>Акции</a>
+	 </div>
+	 
+	 <div class="menubut4">
+	 <a>Контакты</a>
+	 <a>Телeфон</a>
+	 <a>Адрес</a>
+	 <a>Местоположение</a>
+	 </div>
+	 
+	 <div class="zvonki">
+	 
+	 <div class="tel"><b>+7(915) 715-64-53</b></div>
+	 <div class="zakZV">заказать звонок</div>
+	 </div>
+	 </div>
+	
+	<div></div>
+	<div class="emptyblock"></div>
+
+	
+	
+	</body>
+
+	
+</html>
